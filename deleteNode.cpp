f4651472a7a63abf95249c30d0ddfac43c5f122c
Sarbjot  Singh <75158735+sarbjotsingh1@users.noreply.github.com>
2023-07-03 22:46:17 +0530

    Create deleteNode.cpp

---
+++ deleteNode.cpp
@@ -0,0 +1,29 @@
+//https://www.codingninjas.com/studio/problems/delete-node-in-a-linked-list_8230813?challengeSlug=striver-sde-challenge&leftPanelTab=0
+
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    node->data=node->next->data;
+    node->next=node->next->next;
+
+}
