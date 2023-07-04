6fd8c7cf4292df5a99b80844361988b9717f07dc
Sarbjot  Singh <75158735+sarbjotsingh1@users.noreply.github.com>
2023-07-04 15:06:09 +0530

    Create preorder.cpp

---
+++ preorder.cpp
@@ -0,0 +1,32 @@
+//https://www.codingninjas.com/studio/problems/preorder-traversal_8230856?challengeSlug=striver-sde-challenge&leftPanelTab=0
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+
+void solve(TreeNode *root, vector<int>& ans){
+    if(root == nullptr)
+        return;
+
+    ans.push_back(root->data);
+    solve(root->left,ans);
+    solve(root->right,ans);
+}
+
+vector<int> getPreOrderTraversal(TreeNode *root){
+    // Write your code here.
+    vector<int>  ans;
+
+    solve(root,ans);
+    return ans;
+
+}
